3904cf3fabd1a571ad3ee4d37fc471c562eb07c7
Island-c <liu0148@outlook.com>
2019-04-22 12:07:38 +0800

    Update formatTrans.java

--- src/formatTrans/formatTrans.java
+++ src/formatTrans/formatTrans.java
@@ -19,47 +19,47 @@ public class formatTrans
         //String inputFileName = "F:\\database\\pdd_nt\\a.nt";
 		//String inputFileName = "F:\\database\\pdd_nt\\age_gender.nt";
 		String inputFileName = "F:\\database\\LOL\\kglol.rdf";
-		
-		
+
+
 		Model model = ModelFactory.createDefaultModel();
- 
+
 		InputStream in =FileManager.get().open(inputFileName);
-		if (in == null) 
+		if (in == null)
 		{
 			throw new IllegalArgumentException("File: " + inputFileName + " not found");
 		}
- 
-		
+
+
 		//根据读取文件的格式，设置read函数的参数，预定义的值有："RDF/XML","RDF/XML-ABBREV","N-TRIPLE","N3"。默认值参数是""(空值)，对应的格式为：RDF/XML.
         model.read(in, "","RDF/XML");
 		//model.read(in, "","N3");
 		//model.read(in, "","TTL");
- 
+
 		// 使用迭代器遍历
 		StmtIterator iter = model.listStatements();
 		int i=0; //控制输出数量
-		while (iter.hasNext()) 
+		while (iter.hasNext())
 		{
 			Statement stmt = iter.nextStatement(); // get next statement
-			
-			
+
+
 			//Resource subject = stmt.getSubject(); // get the subject
 			//Property predicate = stmt.getPredicate(); // get the predicate
 			//RDFNode object = stmt.getObject(); // get the object
- 
+
 			String subject = stmt.getSubject().toString(); // get the subject
 			String predicate = stmt.getPredicate().toString(); // get the predicate
 			RDFNode object = stmt.getObject(); // get the object
-			
+
 			String[] sub = subject.split("#");
 			//String sub_one=sub[sub.length-1];
 			//sub=sub_one.split("/");
-			System.out.print("【主语】 " + sub[sub.length-1]);	
+			System.out.print("【主语】 " + sub[sub.length-1]);
 			String[] pre = predicate.split("#");
-			System.out.print("  【谓语】 " + pre[pre.length-1]);	
-			
+			System.out.print("  【谓语】 " + pre[pre.length-1]);
+
 			//宾语可能是文本，也可能是实体
-			if (object instanceof Resource) 
+			if (object instanceof Resource)
 			{
 				String[] obj=object.toString().split("#");
 				System.out.print("  【宾语】[resource] " + obj[obj.length-1]);
@@ -68,8 +68,8 @@ public class formatTrans
 				String[] obj=object.toString().split("E");
 				System.out.print("  【宾语】[literal] " + obj[0] );
 			}
-			System.out.println(""); 
-			
+			System.out.println("");
+
 			if(i++ ==500)
 				//读取十条看看....
 				return;
